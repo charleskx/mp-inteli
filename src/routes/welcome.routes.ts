21806bdd0f44878bb7c8fb7add21682c1c0b8e0e
Charleston Amaral <charleston.silva03@gmail.com>
2022-08-10 17:01:35 -0300

    feat: remove welcome

--- src/routes/welcome.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-const welcomeRoutes = Router();
-
-welcomeRoutes.get("/", (request, response) => {
-  response.json({ message: "Inteli" });
-});
-
-export { welcomeRoutes };
--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,15 @@ import cors from "cors";
 import express from "express";
 
 import { paymentsRoutes } from "./routes/payments.routes";
-import { welcomeRoutes } from "./routes/welcome.routes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/", welcomeRoutes);
+app.get("/", (_, response) => {
+  response.json({ message: "welcome inteli" });
+});
 app.use("/payments", paymentsRoutes);
 
 app.listen(3333, () => console.log("Server is running!"));
